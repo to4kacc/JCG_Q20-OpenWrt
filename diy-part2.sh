169cc6ccb8d064c6614de943ea0c06e59b406f23
to4kacc <142129784+to4kacc@users.noreply.github.com>
2025-03-27 14:12:18 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -4,6 +4,16 @@
 #=================================================             
 
 
+##
+echo -e "\nmsgid \"Control\"" >> feeds/luci/modules/luci-base/po/zh_Hans/base.po
+echo -e "msgstr \"控制\"" >> feeds/luci/modules/luci-base/po/zh_Hans/base.po
+
+echo -e "\nmsgid \"NAS\"" >> feeds/luci/modules/luci-base/po/zh_Hans/base.po
+echo -e "msgstr \"网络存储\"" >> feeds/luci/modules/luci-base/po/zh_Hans/base.po
+
+echo -e "\nmsgid \"VPN\"" >> feeds/luci/modules/luci-base/po/zh_Hans/base.po
+echo -e "msgstr \"魔法网络\"" >> feeds/luci/modules/luci-base/po/zh_Hans/base.po
+
 ##配置IP
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
@@ -15,7 +25,10 @@ rm -rf ./feeds/extraipk/theme/luci-theme-edge
 rm -rf ./feeds/extraipk/theme/luci-theme-ifit
 rm -rf ./feeds/extraipk/theme/luci-theme-opentopd
 rm -rf ./feeds/extraipk/theme/luci-theme-neobird
+
 rm -rf ./feeds/extraipk/patch/luci-app-turboacc
+rm -rf ./feeds/extraipk/patch/autocore
+
 
 rm -rf ./package/feeds/extraipk/luci-theme-argon-18.06
 rm -rf ./package/feeds/extraipk/luci-app-argon-config-18.06
@@ -24,7 +37,9 @@ rm -rf ./package/feeds/extraipk/theme/luci-theme-edge
 rm -rf ./package/feeds/extraipk/theme/luci-theme-ifit
 rm -rf ./package/feeds/extraipk/theme/luci-theme-opentopd
 rm -rf ./package/feeds/extraipk/theme/luci-theme-neobird
+
 rm -rf ./package/feeds/extraipk/luci-app-turboacc
+rm -rf ./package/feeds/extraipk/autocore
 
 
 ##取消bootstrap为默认主题
@@ -38,19 +53,16 @@ sed -i "s/hostname='.*'/hostname='AE86Wrt'/g" package/base-files/files/bin/confi
 
 
 ##WiFi
-# sed -i "s/ImmortalWrt/AE86/g" package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i "s/MT7986_AX6000_2.4G/AE86-2.4G/g" package/mtk/drivers/wifi-profile/files/mt7986/mt7986-ax6000.dbdc.b0.dat
+sed -i "s/MT7986_AX6000_5G/AE86-5G/g" package/mtk/drivers/wifi-profile/files/mt7986/mt7986-ax6000.dbdc.b1.dat
 
+sed -i "s/MT7981_AX3000_2.4G/AE86-2.4G/g" package/mtk/drivers/wifi-profile/files/mt7981/mt7981.dbdc.b0.dat
+sed -i "s/MT7981_AX3000_5G/AE86-5G/g" package/mtk/drivers/wifi-profile/files/mt7981/mt7981.dbdc.b1.dat
 
-##MosDNS
-rm -rf feeds/packages/net/mosdns/*
-cp -af feeds/extraipk/op-mosdns/mosdns/* feeds/packages/net/mosdns/
-rm -rf feeds/packages/net/v2ray-geodata/*
-cp -af feeds/extraipk/op-mosdns/v2ray-geodata/* feeds/packages/net/v2ray-geodata/
+##New WiFi
+sed -i "s/ImmortalWrt-2.4G/AE86-2.4G/g" package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
+sed -i "s/ImmortalWrt-5G/AE86-5G/g" package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
 
-
-##更新FQ
-rm -rf feeds/luci/applications/luci-app-passwall/*
-cp -af feeds/extraipk/patch/wall-luci/luci-app-passwall/*  feeds/luci/applications/luci-app-passwall/
-
-# rm -rf feeds/luci/applications/luci-app-ssr-plus/*
-# cp -af feeds/extraipk/patch/wall-luci/luci-app-ssr-plus/*  feeds/luci/applications/luci-app-ssr-plus/
+## golang 为 1.24.x
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
